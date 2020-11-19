15c520a1a279e910cefea9f0a768d438b63eb9f6
ie715630 <ie715630@iteso.mx>
2020-11-19 16:04:48 -0600

    Vibrations sensor SW420 working

--- IoT_colmena_sensores.ino
+++ IoT_colmena_sensores.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+const int SW420_GPIO = 15;
 
+void setup() {
+    Serial.begin(115200);
+
+    pinMode(SW420_GPIO, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    bool vibration_value = get_vibration();
+    Serial.println(vibration_value);
+    delay()
+}
 
+bool get_vibration() {
+    return digitalRead(SW420_GPIO);
 }
